12641804fd93c2a0f87d0636d330997bfcb7d904
TreySchneider95 <tschneid95@gmail.com>
2022-02-28 20:54:33 -0700

    day 3

--- task_manager/static/js/main.js
+++ task_manager/static/js/main.js
@@ -2,6 +2,21 @@ let nonImportantClasses = "far fa-star"
 let importantClasses = "fas fa-star"
 let varIsImportant = false
 let varFormHidden = true
+function displayTask(task){
+    let syntax =`<div id="row-task" style="color: ${task.color}; background-color: rgba(234, 245, 244, 0.568);">
+                <div id="left-task">
+                    <h3>${task.title}</h3>
+                    <p>${task.description}</p>
+                </div>
+                <div id="right-task">
+                <label>Due Date: ${task.dueDate}</label>
+                <label>Location: ${task.location}</label>
+                <label>Contact: ${task.contact}</label>
+                </div>
+                <button onclick="this.parentNode.remove()">Remove</button>
+                </div>`
+    $('#tasks-list').append(syntax)
+}
 
 function toggleImportant(){
     if(varIsImportant){
@@ -16,7 +31,6 @@ function toggleImportant(){
 }
 
 function toggleForm(){
-    console.log('here')
     if(varFormHidden){
         $("#form").show('slide')
         $("#showHideBtn").html('Hide Task Form')
@@ -28,11 +42,63 @@ function toggleForm(){
     }
 }
 
+function clearFields(){
+    $('#txtTitle').val('')
+    $('#dateDueDate').val('')
+    $('#txtContact').val('')
+    $('#txtLocation').val('')
+    $('#txtDescription').val('')
+}
+
+function saveTask(e){
+    e.preventDefault()
+    console.log('save form')
+    let title = $('#txtTitle').val()
+    let dueDate = $('#dateDueDate').val()
+    let contact = $('#txtContact').val()
+    let location = $('#txtLocation').val()
+    let description = $('#txtDescription').val()
+    let color = $('#colorColor').val()
+    if(title.length < 1){
+        $('#txtTitle').css("background-color", "red")
+        return
+    }else{
+        $('#txtTitle').css("background-color", "white")
+    }
+    if(!dueDate){
+        $('#dateDueDate').css("background-color", "red")
+        return
+    }else{
+        $('#dateDueDate').css("background-color", "white")
+    }
+    if(contact < 1){
+        $('#txtContact').css("background-color", "red")
+        return
+    }else{
+        $('#txtContact').css("background-color", "white")
+    }
+    if(location.length < 1){
+        $('#txtLocation').css("background-color", "red")
+        return
+    }else{
+        $('#txtLocation').css("background-color", "white")
+    }
+    if(description.length < 1){
+        $('#txtDescription').css("background-color", "red")
+        return
+    }else{
+        $('#txtDescription').css("background-color", "white")
+    }
+    clearFields()
+    let newTask = new Task(title, varIsImportant, dueDate, contact, location, description, color)
+    displayTask(newTask)
+}
 
 function init(){
     console.log('Task manager')
     $("#iImportant").click(toggleImportant)
     $("#showHideBtn").click(toggleForm)
+    $("#saveBtn").click(saveTask)
 }
 
 
